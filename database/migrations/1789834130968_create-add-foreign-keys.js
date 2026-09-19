6d57e5c80e00bc26a7f556af7169528e17ea9c6e
Mariani5472 <vini.mariani.santos@gmail.com>
2026-09-19 13:28:54 -0300

    drop keys

--- database/migrations/1789834130968_create-add-foreign-keys.js
+++ database/migrations/1789834130968_create-add-foreign-keys.js
@@ -136,6 +136,13 @@ export function down(pgm) {
     ALTER TABLE venue_teams
     DROP CONSTRAINT fk_venue_teams_team;
 
+    ALTER TABLE league_most_titles_teams
+    DROP CONSTRAINT fk_league_most_titles_teams_team
 
+    ALTER TABLE team_achievement_seasons
+    drop CONSTRAINT fk_team_achievement_seasons_team
+
+          ALTER TABLE team_achievement_seasons
+drop CONSTRAINT fk_team_achievement_seasons_team
   `);
 }
