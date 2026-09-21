d59ae91ed13dbb71809891e0b6fb5490959786ea
Mariani5472 <vini.mariani.santos@gmail.com>
2026-09-21 17:17:30 -0300

    com .env

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ async function main(): Promise<void> {
   console.log(browser.get(), process.env.BROWSER);
 
   const storage = new JsonStorage(path.resolve(process.cwd(), "data"));
-  storage.setRewrite(true);
+  storage.setRewrite(process.env.REWRITE?.toLocaleLowerCase() === "true");
 
   const scraper = new Scraper(browser, { storage });
   scraper.completeScrap(desiredLeagues);
